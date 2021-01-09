dcd7876e389bdac5e23aedafcd3b8a151178393a
Cedric Mendes <cmendes062@gmail.com>
2021-01-09 14:17:21 -0500

    added Environmental Variables for testing

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return "Hello Stagers"
+    return "Hello Development People"
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS']) # Sanity Check
     app.run()
+    
